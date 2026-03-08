81542d2598f06f13b77fee7b68a25916ae43d3d7
asang76 <asangtamgadge70@gmail.com>
2026-03-08 15:04:22 +0530

    added routes

--- app/(protected)/index.tsx
+++ app/(protected)/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to </Text>
+      <Text>Home Screen </Text>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,39 @@
+import { ClerkProvider, useAuth } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache";
 import { Stack } from "expo-router";
 
+function RootLayoutWithAuth() {
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+  if (!publishableKey) {
+    throw new Error("Add your Clerk Publishable Key to the .env file");
+  }
+  const { isSignedIn, isLoaded } = useAuth();
+  if (!isLoaded) {
+    return null;
+  }
+  if (!isSignedIn) {
+  }
+
+  return (
+    <Stack>
+      <Stack.Protected guard={isSignedIn}>
+        <Stack.Screen name="(protected)" />
+      </Stack.Protected>
+      <Stack.Protected guard={!isSignedIn}>
+        <Stack.Screen name="(public)" />
+      </Stack.Protected>
+    </Stack>
+  );
+}
+
 export default function RootLayout() {
-  return <Stack />;
+  const publishkey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+  if (!publishkey) {
+    return "";
+  }
+  return (
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishkey}>
+      <RootLayoutWithAuth />
+    </ClerkProvider>
+  );
 }
